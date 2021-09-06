ea83f562abaa0b8569e9cbf0527a3cf16abfd9fb
Roberto Iglesias Cuesta <41485375+Ricuesta86@users.noreply.github.com>
2021-09-06 11:04:16 -0400

    formulario hooks formulario con hooks

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Form from "./components/Form";
 export default function App() {
   return (
     <div>
-      hola
       <Form />
     </div>
   );
 }
+// 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
 export default function Form() {
-  return <h1>Form</h1>;
+  const initState = {
+    input: ""
+  };
+  const [inputState, setInputState] = useState(initState);
+
+  const handleChange = (event) => {
+    // return
+    setInputState({
+      input: "Hola"
+    });
+  };
+  return (
+    <>
+      <form>
+        <h1>Formulario</h1>
+        <input
+          type="text"
+          name="input"
+          onChange={handleChange}
+          value={inputState.input}
+        />
+      </form>
+    </>
+  );
 }
